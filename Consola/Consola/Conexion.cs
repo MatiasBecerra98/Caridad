56029b8748a72c757aca7432b171c9a904d4155a
MatiasBecerra98 <39144419+MatiasBecerra98@users.noreply.github.com>
2018-05-31 15:18:27 -0300

    primer commit

---
+++ Consola/Consola/Conexion.cs
@@ -0,0 +1,173 @@
+﻿using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+
+namespace Consola
+{
+    public class Conexion
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+        private string SslMode;
+        public Conexion()
+        {
+            Initialize();
+        }
+
+        //Initialize values
+        private void Initialize()
+        {
+            server = "localhost";
+            database = "duoc";
+            uid = "root";
+            password = "";
+            SslMode = "none";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";" + "SslMode=" + SslMode+ ";";
+
+            connection = new MySqlConnection(connectionString);
+        }
+
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("No se pudo conectar"+ex.Message);
+                return false;
+            }
+        }
+
+        //Close connection
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        //Insert statement
+        public void Insert(string query)
+        {
+            if (this.OpenConnection() == true)
+            {
+                //create command and assign the query and connection from the constructor
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                //Execute command
+                cmd.ExecuteNonQuery();
+
+                //close connection
+                this.CloseConnection();
+            }
+        }
+
+        //Update statement
+        public void Update(string query)
+        {
+            if (this.OpenConnection() == true)
+            {
+                //create mysql command
+                MySqlCommand cmd = new MySqlCommand();
+                //Assign the query using CommandText
+                cmd.CommandText = query;
+                //Assign the connection using Connection
+                cmd.Connection = connection;
+
+                //Execute query
+                cmd.ExecuteNonQuery();
+
+                //close connection
+                this.CloseConnection();
+            }
+        }
+
+        //Delete statement
+        public void Delete(string query)
+        {
+
+            if (this.OpenConnection() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                cmd.ExecuteNonQuery();
+                this.CloseConnection();
+            }
+        }
+
+        //Select statement
+        public List<User> Select(string query)
+        {
+
+
+            //Create a list to store the result
+            List<User> users = new List<User>();
+
+            //Open connection
+            if (this.OpenConnection() == true)
+            {
+                //Create Command
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                //Create a data reader and Execute the command
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                //Read the data and store them in the list
+                while (dataReader.Read())
+                {
+                    User us = new User();
+                    us.id = int.Parse((dataReader["id"] + ""));
+                    us.nombre=(dataReader["nombre"] + "");
+                    us.apellido=(dataReader["apellido"] + "");
+                    us.rut = (dataReader["rut"] + "");
+                    users.Add(us);
+                }
+
+                //close Data Reader
+                dataReader.Close();
+
+                //close Connection
+                this.CloseConnection();
+
+                //return list to be displayed
+                return users;
+            }
+            else
+            {
+                return users;
+            }
+        }
+
+        ////Count statement
+        //public int Count()
+        //{
+        //}
+
+        ////Backup
+        //public void Backup()
+        //{
+        //}
+
+        ////Restore
+        //public void Restore()
+        //{
+        //}
+
+    }
+}
---
+++ Consola/Consola/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mostar();
+        }
+
+        private static void mostar()
+        {
+            Conexion conexion = new Conexion();
+            List<User> listita = new List<User>();
+            listita = conexion.Select("SELECT * FROM prueba");
+
+            foreach (User item in listita)
+            {
+                Console.WriteLine("ID : {0} \nNombre : {1} \nApellido : {2} \n Rut : {3}",item.id,item.nombre,item.apellido,item.rut);
+            }
+            Console.ReadKey();
+        }
+    }
+}
